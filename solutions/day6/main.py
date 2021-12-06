6450eb3d3650e8c650acf0f01d19300b92d7f88d
Richard Baumann <ohaz@ohaz.engineer>
2021-12-06 07:41:04 +0100

    Replace bad solution for task 1 with good solution

--- solutions/day6/main.py
+++ solutions/day6/main.py
@@ -11,15 +11,11 @@ def get_input():
 
 def task1():
     fishes = [int(x) for x in get_input()[0].split(',')]
+    fish_groups = deque([fishes.count(x) for x in range(9)])
     for _ in range(80):
-        for index, f in enumerate(fishes[:]):
-            f -= 1
-            if f >= 0:
-                fishes[index] = f
-            else:
-                fishes[index] = 6
-                fishes.append(8)
-    return len(fishes)
+        fish_groups.rotate(-1)
+        fish_groups[6] += fish_groups[8]
+    return sum(list(fish_groups))
 
 def task2():
     fishes = [int(x) for x in get_input()[0].split(',')]
