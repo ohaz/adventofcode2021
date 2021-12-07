a8a57a05bb51b8607455b09105cd2a667e19d82f
Richard Baumann <ohaz@ohaz.engineer>
2021-12-07 06:53:48 +0100

    Add hacky solution for day7

---
+++ solutions/day7/main.py
@@ -0,0 +1,30 @@
+import os
+
+def get_example_input():
+    s = """16,1,2,0,4,2,7,1,2,14"""
+    return s.splitlines()
+
+def get_input():
+    with open('solutions/day7/input.txt') as f:
+        return [x.strip() for x in f.readlines()]
+
+def task1():
+    crabs = [int(x) for x in get_input()[0].split(',')]
+    best_fuel_cost = 999999999999
+    for pos in range(min(crabs), max(crabs) + 1):
+        fuel_cost = 0
+        for crab in crabs:
+            fuel_cost += abs(crab - pos)
+        best_fuel_cost = min(best_fuel_cost, fuel_cost)
+    return best_fuel_cost
+        
+
+def task2():
+    crabs = [int(x) for x in get_input()[0].split(',')]
+    best_fuel_cost = 999999999999
+    for pos in range(min(crabs), max(crabs) + 1):
+        fuel_cost = 0
+        for crab in crabs:
+            fuel_cost += sum(range(abs(crab - pos) + 1))
+        best_fuel_cost = min(best_fuel_cost, fuel_cost)
+    return best_fuel_cost
