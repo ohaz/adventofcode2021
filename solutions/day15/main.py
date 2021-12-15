d0cc3e2a8152ed6e98fbaecce426e3efa327f765
Richard Baumann <ohaz@ohaz.engineer>
2021-12-15 09:50:30 +0100

    Use networkx EVERYWHERE

--- solutions/day15/main.py
+++ solutions/day15/main.py
@@ -28,40 +28,6 @@ def get_neighbour_locations(x, y):
 def is_valid_neighbour(world, x, y):
     return x >= 0 and y >= 0 and x < len(world[0]) and y < len(world)
 
-class Graph:
-    def __init__(self, amount_vertices):
-        self.amount_vertices = amount_vertices
-        self.edges = [[-1 for i in range(amount_vertices)] for j in range(amount_vertices)]
-        self.visited = set()
-
-    def add_edge(self, u, v, u_v_weight, v_u_weight):
-        self.edges[u][v] = u_v_weight
-        self.edges[v][u] = v_u_weight
- 
-    def dijkstra(self, src):
-        D = {v:sys.maxsize for v in range(self.amount_vertices)}
-        D[src] = 0
-
-        queue = PriorityQueue()
-        queue.put((0, src))
-
-        while not queue.empty():
-            (distance, current) = queue.get()
-            self.visited.add(current)
-
-            for neighbour in range(self.amount_vertices):
-                if self.edges[current][neighbour] != -1:
-                    distance = self.edges[current][neighbour]
-                    if neighbour not in self.visited:
-                        old_cost = D[neighbour]
-                        new_cost = D[current] + distance
-
-                        if new_cost < old_cost:
-                            queue.put((new_cost, neighbour))
-                            D[neighbour] = new_cost
-        return D
-
-
 def task1():
     world = []
     for line in get_input():
@@ -69,7 +35,7 @@ def task1():
         for entry in line:
             world[-1].append(int(entry))
     
-    graph = Graph(len(world) * len(world[0]))
+    graph = networkx.DiGraph()
 
     for y in range(len(world)):
         for x in range(len(world[0])):
@@ -77,11 +43,10 @@ def task1():
             for neighbour in neighbours:
                 _x, _y = neighbour
                 if is_valid_neighbour(world, _x, _y):
-                    graph.add_edge(y * len(world[0]) + x, _y * len(world[0]) + _x,  world[_y][_x], world[y][x])
+                    graph.add_edge((x, y), (_x, _y), weight=world[_x][_y])
+                    graph.add_edge((_x, _y), (x, y), weight=world[x][y])
 
-    distances = graph.dijkstra(0)
-    target_location = len(world) * len(world[0]) - 1
-    return distances[target_location]
+    return networkx.shortest_path_length(graph, source=(0,0), target=(len(world[0]) -1, len(world) - 1), weight='weight')
 
 def task2():
     world = []
